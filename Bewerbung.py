cfe71af0a9e6bc7b2d0cf86acd55a637cf2e3698
Patrick Carillo <p.carillo17@googlemail.com>
2026-04-25 23:33:49 +0200

    Design pivot: D&D structure for the job  application

--- Bewerbung.py
+++ Bewerbung.py
@@ -1,30 +1,74 @@
 class bewerber:
     def __init__(self,
-                 name,
-                 position,
-                 company,
-                 tech_stack,
-                 duration
+                name: str,
+                 char_class: str,
+                 level: int,
+                 stats: dict[str, int],
+                 proficiencies: list[str],
+                 inventory: list[str],
+                 backstory: str,
+                 quest: str
     ) -> None:
         self.name = name
-        self.position = position
-        self.company = company
-        self.tech_stack = tech_stack
-        self.duration = duration
+        self.char_class = char_class
+        self.level = level
+        self.stats = stats
+        self.proficiencies = proficiencies
+        self.inventory = inventory
+        self.backstory = backstory
+        self.quest = quest
 
-def main() -> None:
-    bewerbung = bewerber(
+
+def main():
+    pc = bewerber(
         name="Patrick Carillo",
-        position="Praktikum Softwareentwicklung C#/.NET",
-        company="Lorent IT-Lösungen GmbH",
-        tech_stack=["Python", "LUA", "Git", "SQL", "HTML"], # TODO explain the level as mostly basic
-        duration="April 2027 bis Oktober 2027"
-    )
+        char_class="Apprentice Software Developer",
+        level=1,
+        stats={
+            "Lernbereitschaft":       17,
+            "Motivation":             14,
+            "Technik-Begeisterung":   14,
+            "Kommunikation":          16,
+            "Schlafbedarf":           6
+        },
+        proficiencies=["Python", "Lua", "Git", "SQL", "HTML"],
+        inventory=[
+            "Mechanische Tastatur +1",
+            "Cheat-Sheet der Pythonbefehle",
+            "Trank der säurigen Kohlenwasser"
+        ],
+        backstory=(
+            "Patrick begann seine Reise in einem Studium der 3D-Animation mit\n"
+            " dem Fokus auf Game Development. Dieser Weg war ein Resultat von\n"
+            " früher Begegnung von Technologie (C64, Win 95 & Suse Linux waren die\n"
+            " ersten Betriebssysteme), Informatiklehrer (brachte ihm Scratch und HTML\n"
+            " näher) und Affinität zu Videospielen sowie Bedürfnis zum kreativem Output."
+        ),
+        quest=(
+            "Eine Anstellung als Praktikant bei der Lorent IT-Lösungen GmbH"
+            "im Reich der C#/Net-Entwicklung, von April bis Oktober 2027."
+        )
+     )
 
-    print(f"Sehr geehrtes Team  der {bewerbung.company},\n")
-    print(f"mein Name ist {bewerbung.name} und dies ist meine Bewerbung")
-    print(f"auf die Stelle {bewerbung.position} im Zeitraum {bewerbung.duration}.")
+    print(f"=== Charakterbogen ===")
+    print(f"Name: {pc.name}")
+    print(f"Klasse: {pc.char_class} (Stufe {pc.level})\n")
 
+    print("Attribute")
+    for stat, value in pc.stats.items():
+        print(f"{stat:.<25} {value}")
+    print()
+
+    print(f"Fertigkeiten: {', '.join(pc.proficiencies)}\n")
+
+    print("Inventar")
+    for item in pc.inventory:
+        print(f"- {item}")
+    print()
+
+    print(f"Hintergrund:\n {pc.backstory}\n")
+    print(f"Aktuelle Quest:\n {pc.quest} ")
 
 if __name__ == "__main__":
     main()
+
--- readme.md
+++ readme.md
@@ -2,12 +2,14 @@
 
 Willkommen! Diese Repo enthält meine Bewerbung für das
 **Praktikum in d. Softwareentwicklung** bei der Lorent IT-Lösungen GmbH
-im Zeitraum April 2027 bis Oktober 2027.
+im Zeitraum April 2027 bis Oktober 2027. 
 
 ## Warum auf diese Art?
 
 Inspiriert von dem Herrn Lorent und seiner Präsentation an der BBQ setzte
-ich es mir zum Ziel das Anschreiben zu individualisieren.
+ich es mir zum Ziel das Anschreiben zu individualisieren, dabei nahm ich ein 
+momentanes Coding-Projekt eines HIlfstool für Dungeons & Dragons als Idee für
+dioe Gestaltung der BEwerbung und insbesondere des Anschreibens. 
 
 ## Requirements
 
@@ -21,7 +23,7 @@ python bewerbung.py
 
 ## Wie kam ich zur Softwareentwicklung?
 
-Der
+
 
 
 ## Meine Erfahhrung außerhalb der Umschulung
@@ -29,6 +31,10 @@ Der
 
 ## Aktueller Tech-Stack
 
+| Sprache/Tool | Level |
+|--------------|-------|
+|Python        | Basics|
+|
 
 ## Die Vorstellung meines Traumberufes
 
